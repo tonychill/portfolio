4b17682dcd8871c8ab59f0d0b00cde9d6861cb1b
Tony Hill <tony@ilanlyfe.com>
2020-09-20 17:50:43 -0400

    Test commit 8s7sh

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,15 +1,14 @@
-import MainFeed from '../components/content/mainfeed';
-import MainCta from '../components/content/maincta';
+import MainFeed from "../components/content/mainfeed";
+import MainCta from "../components/content/maincta";
 
 export default function Home(props) {
-  props.handleTitle('Home');
+  props.handleTitle("Home");
   // console.log(stories);
 
   return (
-    <main className='ts-mw-1280 ts-mt-80'>
+    <main className="ts-mw-1280 ts-mt-80">
       <MainCta />
       <MainFeed />
-      {/* Testing git */}
     </main>
   );
 }
